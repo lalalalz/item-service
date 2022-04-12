bcc9a206f91ef6d1ea93ee2d55680fe403439534
lalalalz <lalalalz@naver.com>
2022-04-12 16:27:15 +0900

    #ADD
    #ADD PRG 패턴 적용 및 RedirectAttributes 적용

--- main/java/hello/itemservice/domain/item/basic/BasicItemController.java
+++ main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -26,7 +27,8 @@ public class BasicItemController {
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable Long itemId, Model model) {
+    public String item(@PathVariable Long itemId,
+                       Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
@@ -54,16 +56,30 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    //    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, Model model) {
+        itemRepository.save(item);
+      return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV4(Item item, Model model) {
         itemRepository.save(item);
         return "basic/item";
     }
 
-    @PostMapping("/add")
-    public String addItemV4(Item item, Model model) {
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
         itemRepository.save(item);
-        return "basic/item";
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
@@ -81,7 +97,6 @@ public class BasicItemController {
 
     @PostConstruct
     public void init() {
-
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
     }
